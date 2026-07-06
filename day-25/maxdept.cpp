481836dc2c2491940fd76eb3f669d7a23e46a2bd
indrajeet badhel <indrajeetnareshbadhel@gmail.com>
2026-07-06 21:24:10 +0530

    Add maxdept.cpp

---
+++ day-25/maxdept.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+int maxDepthhelper(TreeNode* root) {
+
+    if(root == nullptr)
+        return 0;
+
+    int left = maxDepthhelper(root->left);
+    int right = maxDepthhelper(root->right);
+
+    return 1 + max(left, right);
+}
+
+
+    int maxDepth(TreeNode* root) {
+        return maxDepthhelper(root);
+    }
+};
