4a243111050428870b04591412946f986ca2af8e
indrajeet badhel <indrajeetnareshbadhel@gmail.com>
2026-07-06 21:24:06 +0530

    Add firstbadversion.cpp

---
+++ day-25/firstbadversion.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int ans = n;
+        int l = 1;
+        int h = n;
+
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+
+            if (isBadVersion(mid)) {
+                ans = mid;
+                h = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
